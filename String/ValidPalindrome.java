e41d76b584f339d06cc92fb65aa9819caf8beb7c
Gokul Selvakumar <gokulgokul0264@gmail.com>
2026-06-16 15:55:48 +0530

    Solved Valid Palindrome - String - Easy - LeetCode#125

---
+++ String/ValidPalindrome.java
@@ -0,0 +1,32 @@
+package String;
+
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-z0-9]", "");
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        
+        String s = "A man, a plan, a canal: Panama";
+        ValidPalindrome v = new ValidPalindrome();
+        System.out.println(v.isPalindrome(s));
+    }
+}
+
